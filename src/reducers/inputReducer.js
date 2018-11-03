575670809b8a97e17bbd3d67fef42cecf8d02363
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2018-11-03 13:06:59 -0400

    update initial markdown in textarea to pass tests

--- src/reducers/inputReducer.js
+++ src/reducers/inputReducer.js
@@ -1,6 +1,5 @@
 const defaultState = {
-  init:
-    "# Heading One \n\n## Heading Two \n\n`<div>`\n\n```<div> Hello There</div>```\n\n[I'm an inline-style link](https://www.google.com)\n\n > This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can *put* **Markdown** into a blockquote.\n\n <img src='https://via.placeholder.com/150C'/>\n\n**asterisks**"
+  init: "# Heading One \n\n## Heading Two \n\n`<div>`\n\n``` \n\nSome Code ```\n\n[I'm an inline-style link](https://www.google.com)\n\n >> This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can *put* **Markdown** into a blockquote.\n\n 1. First Item \n\n 2. Second Item \n\n\n\n ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png 'Logo Title Text 1')\n\n\n\n**asterisks**"
 };
 
 export default (state = defaultState, action) => {
